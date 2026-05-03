adb7214900aa6f9bd1fafecf747610386b77ab87
Shouryya Manna <shouryyamanna06@gmail.com>
2026-05-03 12:42:08 +0530

    Native css init

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
+      className="bg-red-400 h-full w-full"
       style={{
         flex: 1,
         justifyContent: "center",
@@ -10,6 +12,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/login">Go to Login</Link>
     </View>
   );
 }
---
+++ app/login/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Login = () => {
+  return (
+    <View className="bg-red-400">
+      <Text className="text-red-600">Login</Text>
+    </View>
+  );
+};
+
+export default Login;
